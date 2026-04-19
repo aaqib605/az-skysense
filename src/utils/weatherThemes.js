277a638ff84e6868a759645a1acc4e4b26229619
cookie-kun <piyushcalling09@gmail.com>
2026-04-20 01:04:33 +0530

    minor fix

--- src/utils/weatherThemes.js
+++ src/utils/weatherThemes.js
@@ -1,33 +1,31 @@
-// function getWeatherTheme(weather) {
-//   if (!weather?.condition) {
-//     return "weather-theme-default";
-//   }
-
-//   const condition = weather.condition.toLowerCase();
-
-//   if (condition === "clear") {
-//     return "weather-theme-clear";
-//   }
-
-//   if (condition === "clouds") {
-//     return "weather-theme-clouds";
-//   }
-
-//   if (condition === "rain" || condition === "drizzle") {
-//     return "weather-theme-rain";
-//   }
-
-//   if (condition === "snow") {
-//     return "weather-theme-snow";
-//   }
-
-//   if (condition === "thunderstorm") {
-//     return "weather-theme-thunderstorm";
-//   }
-
-//   return "weather-theme-default";
-// }
 function getWeatherTheme(weather) {
-  return "weather-theme-thunderstorm";  
+  if (!weather?.condition) {
+    return "weather-theme-default";
+  }
+
+  const condition = weather.condition.toLowerCase();
+
+  if (condition === "clear") {
+    return "weather-theme-clear";
+  }
+
+  if (condition === "clouds") {
+    return "weather-theme-clouds";
+  }
+
+  if (condition === "rain" || condition === "drizzle") {
+    return "weather-theme-rain";
+  }
+
+  if (condition === "snow") {
+    return "weather-theme-snow";
+  }
+
+  if (condition === "thunderstorm") {
+    return "weather-theme-thunderstorm";
+  }
+
+  return "weather-theme-default";
 }
+
 export default getWeatherTheme;
